17ddeb9f6175d753b2dbacd69badd610f3dd2ec4
Martin Owen <martinowenuk@gmail.com>
2009-12-06 21:00:21 +0000

    Added the 'published' flag to every blog post, and included it as an option in the find_posts helper.

--- lib/posts_helper.rb
+++ lib/posts_helper.rb
@@ -1,12 +1,15 @@
 module PostsHelper
 
-  def find_posts
-    limit = 10
+  BLOG_POST_LIMIT = 10
+  HIDE_UNPUBLISHED = false
+
+  def find_posts(limit=BLOG_POST_LIMIT)
     options = { :in_directory => 'blog', 
                 :recursive => true,
                 :sort_by => "created_at",
                 :reverse => true,
                 :blog_post => true }
+    options[:published] = true if HIDE_UNPUBLISHED
     ::Webby::Resources.pages.find(limit, options)
   end
 
