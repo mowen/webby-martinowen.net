219fe2e863cafc15a366189c5f45a5ccb81369ee
Martin Owen <martinowenuk@gmail.com>
2009-12-10 20:08:03 +0000

    Posts are now displayed differently depending on how they are rendered. Posts in a list have a short date and title on the same line. Individual posts have their title in the main heading, and include a long date, and a summary of the post. Also replaced all of the '::' with '//'.

--- lib/posts_helper.rb
+++ lib/posts_helper.rb
@@ -13,6 +13,33 @@ module PostsHelper
     ::Webby::Resources.pages.find(limit, options)
   end
 
+  def format_post_date_short(date)
+    return date.strftime('%Y/%m/%d')
+  end
+
+  def format_post_date_long(date)
+    day = format_day(date.day)
+    return date.strftime("%A the #{day} of %B %Y")
+  end
+
+  private
+
+    def format_day(day_of_month)
+      day_suffix = ""
+
+      if day_of_month == 1
+        day_suffix = "st"
+      elsif day_of_month == 2
+        day_suffix = "nd"
+      elsif day_of_month == 3
+        day_suffix = "rd"
+      else
+        day_suffix = "th"
+      end
+
+      return day_of_month.to_s + day_suffix
+    end
+
 end
 
 Webby::Helpers.register(PostsHelper)
