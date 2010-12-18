1aa4374cbd34b79b5382ef8714028e257c50ff57
Martin Owen <martinowenuk@gmail.com>
2010-12-18 08:47:45 +0000

    Continued with Git Kata, and added a JavaScript file.

---
+++ content/scripts/site.js
@@ -0,0 +1,22 @@
+(function($){
+    var showSolution = function() {
+	var p = $(this); 
+	p.find('a').text('Hide Solution');
+	p.next('.solution').slideDown();
+    };
+
+    var hideSolution = function() {
+	var p = $(this); 
+	p.find('a').text('Show Solution');
+	p.next('.solution').slideUp();
+    };
+
+    $(document).ready(function() {
+	$('.solution').hide()
+	    .each(function() {
+		$("<p><a>Show Solution</a></p>")
+		    .insertBefore(this)
+		    .toggle(showSolution, hideSolution);
+	    });
+    });
+})(jQuery);
